eb80c1fa18ae6fd5cecfc649626dd03330aa60be
Akash Mondal <akash2003modal@gmail.com>
2024-07-18 01:12:39 +0530

    Update configuration and set cookie

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Req, Res, Version } from '@nestjs/common';
 import { AppService } from './app.service';
 import { FastifyReply, FastifyRequest } from 'fastify';
+import configuration from './configs/configuration';
 @Controller({
   version: ['1'],
 })
@@ -15,17 +16,20 @@ export class AppController {
 
   @Version('1')
   @Get('cookie')
-  cookieSet(@Req() request: FastifyRequest,@Res({ passthrough: true }) response: FastifyReply): any {
+  cookieSet(@Req() request: FastifyRequest, @Res({ passthrough: true }) response: FastifyReply): any {
     return response.send(request.cookies)
   }
 
   @Version('1')
   @Get("cookie-set")
   findAll(@Res({ passthrough: true }) response: FastifyReply) {
-    response.setCookie('test-cookie', 'pompom',{
-      domain:"localhost",
-      path:"/"
+    response.setCookie('test', 'test_page', {
+      domain: configuration().DOMAIN,
+      path: "/",
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      httpOnly: true,
+      priority: "medium"
     })
-    return response.send(`Cookie value: ${`pompom`}`)
+    return response.send(`Cookie value: ${`value`}`)
   }
 }
