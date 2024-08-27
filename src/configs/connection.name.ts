ea0991dbc1ae395796639be4a616a0aebcdca16a
Akash Mondal <akash2003mondal.com>
2024-08-28 00:23:49 +0530

    Update package.json and dependencies

--- src/configs/connection.name.ts
+++ src/configs/connection.name.ts
@@ -1,12 +1,14 @@
-export const event_name = {
-    conversation: {
-        message: "conversation_message",
-        seen: "conversation_message_seen",
-        typing: "conversation_user_keyboard_pressing",
-        listRefetch: "conversation_list_refetch",
-    },
-    notification: {
-        post:"notification_post",
-        followRequest: {}
-    },
-}
+export const event_name = Object.freeze(
+    {
+        conversation: {
+            message: "conversation_message",
+            seen: "conversation_message_seen",
+            typing: "conversation_user_keyboard_pressing",
+            listRefetch: "conversation_list_refetch",
+        },
+        notification: {
+            post: "notification_post",
+            followRequest: {}
+        },
+    }
+)
