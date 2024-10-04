9dbf8b4f5c8d4227c029fe8a6d9f648404d41a56
Akash Mondal <akash2003mondal@gmail.com>
2024-10-04 21:35:30 +0530

    Update library and Refactor Dockerfile and docker-compose.yaml

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM node:latest
-
 WORKDIR /app
 COPY . .
-RUN npm install
-RUN npm run build
-EXPOSE 3000
+RUN npm install && npm run build
+EXPOSE 5000
 CMD ["npm","run","start"]
--- src/configs/configuration.ts
+++ src/configs/configuration.ts
@@ -3,8 +3,8 @@ export default () => ({
     POST: Number(process.env.PORT ?? 5000),
 
     // Database
-    PG_URL: process.env.PG_URL ?? "postgresql://postgres:password@localhost:5432/postgres",
-    REDIS_URL: process.env.REDIS_URL ?? "redis://localhost:6379",
+    PG_URL: process.env.PG_URL,
+    REDIS_URL: process.env.REDIS_URL,
 
     /// JWT
     JWT_SECRET: process.env.JWT_SECRET ?? "dRYjpuVfGUTCDttLnTON6KBP6q1kDLUD9ocnoVZAx7Ggmr0aMWK0ABw8JbMys",
